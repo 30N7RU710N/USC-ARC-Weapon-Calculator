b51ea510c0347b0eb126a161f00c0a82cb971348
Anson <extentedemail@gmail.com>
2025-12-27 00:10:25 -0800

    final change to my repo

--- calc.py
+++ calc.py
@@ -2,40 +2,42 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder = 'templates')
 General = {
-    "CV" : "",
-    "numberC" : "",
-    "RM" :"" ,
-    "MagneticD" : "",
-    "Arena" : ""
+    "CV" : 0,
+    "numberC" : 0,
+    "RM" :0 ,
+    "MagneticD" : 0,
+    "Arena" : 0
 }
 Drive = {
-    "DmotorKV" : "",
-    "DSaturation" : "",
-    "DGearboxR" : "",
-    "DExternalR" : "",
-    "DDR" : "",
-    "D#motor" : "",
-    "DWheelD" : ""
+    "DmotorKV" : 0,
+    "DSaturation" : 0,
+    "DGearboxR" : 0,
+    "DExternalR" : 0,
+    "DDR" : 0,
+    "Dnummotor" : 0,
+    "DWheelD" : 0
 }
 Weapon = {
-    "WmotorKV" : "",
-    "WRR" : "",
-    "W#motor" : "",
-    "WWeightmotor" : "",
-    "WweaponD" : "",
-    "WTorque" : "",
-    "WSaturation" : "",
-    "WGoalMOI" : ""
+    "WmotorKV" : 0,
+    "WRR" : 0,
+    "Wnummotor" : 0,
+    "WWeightmotor" : 0,
+    "WweaponD" : 0,
+    "WTorque" : 0,
+    "WSaturation" : 0,
+    "WGoalMOI" : 0
 }
 Arm = {
-    "AMotorKV" : "",
-    "AGearboxR" : "",
-    "ABeltR" : "",
-    "ABevelR" : "",
-    "ASaturation" : "",
-    "AMaxSwingA" : "",
-    "ArmLength" : "",
-    "AWeaponW" : "",
+    "AMotorKV" : 0,
+    "AGearboxR" : 0,
+    "ABeltR" : 0,
+    "ABevelR" : 0,
+    "ASaturation" : 0,
+    "AMaxSwingA" : 0,
+    "ArmLength" : 0,
+    "AWeaponW" : 0,
+    "ArmWeight" :0,
+    "MOI" : 0
 }
 @app.route('/', methods = ['GET', 'POST'])
 def index():
